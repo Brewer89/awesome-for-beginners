4f48d7c53c8713b6843b2affca04621cfa87f605
Isaac Coddington <ijc1989@hotmail.com>
2025-02-16 19:54:34 -0600

    Added port listener java file port 8082

---
+++ beginner_fork/src/main/java/com/mycompany/beginner_fork/Beginner_fork.java
@@ -0,0 +1,31 @@
+package com.mycompany.beginner_fork;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+//more: https://javarevisited.blogspot.com/2015/06/how-to-create-http-server-in-java-serversocket-example.html#ixzz90TS1XML9
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author ijc19
+ */
+public class Beginner_fork {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(8082);
+        System.out.println("Listening for connection on port 8082 ....");
+        while (true) {
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+            }
+                
+            }
+        }
+    }
+
